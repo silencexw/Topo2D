bb23753dcc64808c58cd97ac00400447623cd9c8
silencexw <2942343833@qq.com>
2024-07-29 18:53:50 +0800

    first

---
+++ mmdet3d_plugin/topo2d/modules/__init__.py
@@ -0,0 +1,10 @@
+from .transformer_maptr import MapTRDecoder2D, MapTRTransformer2D, MapTRTransformer2DMlvl
+from .query_generator import QueryGenerator
+from .transformer_petr import PETRTransformer
+from .position_embedding import SinePositionalEncoding3D, LearnedPositionalEncoding3D
+from .sparse_int import SparseInsDecoderMask
+from .ms2one import build_ms2one, Naive, DilateNaive
+from .attn import FlashMHA
+from .transformer_maptr_bev import MapTRPerceptionTransformer
+from .maptr_decoder import MapTRDecoder
+from .geometry_kernel_attention import GeometrySptialCrossAttention, GeometryKernelAttention
